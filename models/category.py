e60f0ac8cd8e01b5ba2fa3a60756689cb988098e
alexnegrya <82027665+alexnegrya@users.noreply.github.com>
2021-06-23 15:04:54 +0300

    upgraded Category

--- models/category.py
+++ models/category.py
@@ -1,9 +1,10 @@
 class Category:
-    def __init__(self, name, parentCategoryId=None):
-        if type(name) == str:
+    def __init__(self, id, name, parentCategoryId=None):
+        if type(name) == str and type(id) == str:
             self.name = name
+            self.id = id
         else:
-            raise TypeError('wrong name type')
+            raise TypeError('wrong name or id type')
         if parentCategoryId != None:
             if type(parentCategoryId) == str:
                 self.parentCategoryId = parentCategoryId
@@ -14,12 +15,13 @@ class Category:
     
     def __str__(self):
         title = f"--- Category \"{self.name}\" ---"
+        id = f"Id: {self.id}"
         if self.parentCategoryId == None:
             parent = 'Have parent category: No'
         else:
             parent = 'Have parent category: Yes'
             parent = parent + '\n' + f'- parent categoty id: {self.parentCategoryId}'
-        out = title + '\n' + parent
+        out = title + '\n' + id + '\n' + parent
         return out
     
     def __repr__(self):

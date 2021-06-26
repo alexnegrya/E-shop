93f8311950da8f27aa9c1d7695a2be8ad252f03d
alexnegrya <82027665+alexnegrya@users.noreply.github.com>
2021-06-26 18:24:39 +0300

    fixed Category

--- models/Category.py
+++ models/Category.py
@@ -3,15 +3,7 @@ class Category:
         if type(name) == str and type(id) == str:
             self.name = name
             self.id = id
-        else:
-            raise TypeError('wrong name or id type')
-        if parentCategoryId != None:
-            if type(parentCategoryId) == str:
-                self.parentCategoryId = parentCategoryId
-            else:
-                raise TypeError('wrong parentCategoryId type')
-        else:
-            self.parentCategoryId = None
+            self.parentCategoryId = parentCategoryId
     
     def __str__(self):
         title = f"--- Category \"{self.name}\" ---"
@@ -21,7 +13,7 @@ class Category:
         else:
             parent = 'Have parent category: Yes'
             parent = parent + '\n' + f'- parent categoty id: {self.parentCategoryId}'
-        out = title + '\n' + id + '\n' + parent
+        out = '\n' + title + '\n' + id + '\n' + parent + '\n'
         return out
     
     def __repr__(self):

7624dac7e1374845c8b45b3936baaaa81166282a
alexnegrya <82027665+alexnegrya@users.noreply.github.com>
2021-06-28 09:18:48 +0300

    added a class method and ID generator

--- models/Category.py
+++ models/Category.py
@@ -1,9 +1,13 @@
+from random import randint
+
 class Category:
-    def __init__(self, id, name, parentCategoryId=None):
-        if type(name) == str and type(id) == str:
-            self.name = name
-            self.id = id
-            self.parentCategoryId = parentCategoryId
+    def __init__(self, name, parentCategoryId=None):
+        ID = ''
+        for v in range(randint(4, 6)):
+            ID = ID + str(randint(0, 9))
+        self.id = int(ID)
+        self.name = name
+        self.parentCategoryId = parentCategoryId
     
     def __str__(self):
         title = f"--- Category \"{self.name}\" ---"
@@ -18,3 +22,10 @@ class Category:
     
     def __repr__(self):
         return str(self)
+
+    def __setattr__(self, name, value):
+        if name == 'id':
+            if value < 1 or value > 1000000:
+                raise ValueError('id must be greater then 0 and lesser then 1000000')
+            else:
+                object.__setattr__(self, name, value)

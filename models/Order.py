5d5fd84df56a3e573c69c3caab7af2e0ba89d46c
9masha <82033199+9masha@users.noreply.github.com>
2021-06-30 17:51:33 +0200

    Factory added + check 1...1 000 000

--- models/Order.py
+++ models/Order.py
@@ -1,12 +1,14 @@
-import uuid
+
 
 class Order:
-    def __init__(self, itemList, customerId, totalCost, paymentId):
-        self.id = uuid.uuid4()
-        self.itemList = itemList
-        self.customerId = customerId
-        self.totalCost = totalCost
-        self.paymentId = paymentId
+
+    def __init__(self, id, itemList, customerId, totalCost, paymentId):
+        if idValueCheck(id):
+            self.id = id
+            self.itemList = itemList
+            self.customerId = customerId
+            self.totalCost = totalCost
+            self.paymentId = paymentId
 
     def __str__(self):
         return f"\n " \
@@ -18,3 +20,34 @@ class Order:
 
     def __repr__(self):
         return self.__str__()
+
+
+def idValueCheck(id):
+    if 1 <= id < 1000000:
+        return True
+    else:
+        from exceptions.WrongIdValue import WrongIdValueException
+        raise WrongIdValueException
+
+class OrderRepositoryFactory:
+    id_remembered = 0
+
+    def rememberId(self, id_get):
+        self.id_remembered = id_get
+
+    def lastCreatedId(self):
+        if Order is None:
+            lastId = 0
+        else:
+            # global id_remembered
+            lastId = self.id_remembered
+        return lastId
+
+
+    def getOrder(self, itemList, customerId, totalCost, paymentId):
+        id_get = self.lastCreatedId() + 1
+        self.rememberId(id_get)
+        order = Order(id_get, itemList, customerId, totalCost, paymentId)
+        return order
+
+

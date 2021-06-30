6cd8c1cf4de18e45ef6f034059210d65ea1232d8
Ni4ka1991 <astahova_nica@mail.ru>
2021-06-30 13:22:23 +0300

    Removing duplicate code

--- models/OrderItem.py
+++ models/OrderItem.py
@@ -1,19 +1,27 @@
 
 
 #OrderItem.py module
+from random import randint
 
 
 class OrderItem:
     
-    def __init__( self, id, itemld, quantity ):
+    
+    def __init__( self, _id, itemld, quantity ):
+       
+        if _id in range( 1, 1_000_000 + 1 ):
+            self.id = _id
         
-        self.id       = id
+        else:
+            raise ValueError( "id out of range" )
+
         self.itemld   = itemld
         self.quantity = quantity
+
 
     def __str__( self ):
         return f"{self.id} -- {self.itemld} -- {self.quantity}"
 
     def __repr__ ( self ):
-        return f"{self.id} -- {self.itemld} -- {self.quantity}"
-
+        return self.__str__()
+    

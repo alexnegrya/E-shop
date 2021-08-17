ef627fb88f88e6d8486253a8ccce93fbd6de9350
alexnegrya <alexnegrya@mail.ru>
2021-08-17 17:32:56 +0300

    updated boot

--- boot.py
+++ boot.py
@@ -3,6 +3,7 @@ from models.Product import *
 from models.Money import Money
 from models.Order import Order
 from models.Customer import *
+from models.Address import *
 from services.TestData import TestDataService
 
 # Initialization

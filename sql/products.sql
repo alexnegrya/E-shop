0f86bf25c9f802799fcb8f06b15e42d83aa9e216
alexnegrya <alexnegrya@mail.ru>
2021-08-19 15:16:17 +0300

    added FK to products table

--- sql/products.sql
+++ sql/products.sql
@@ -7,5 +7,11 @@ CREATE TABLE products
     category_id integer NOT NULL,
     price_id integer NOT NULL,
     -- CONSTRAINTS/KEYS
-    CONSTRAINT products_pkey PRIMARY KEY (id)
+    CONSTRAINT products_pkey PRIMARY KEY (id),
+    CONSTRAINT products_category_fkey
+        FOREIGN KEY (category_id)
+            REFERENCES categories(id),
+    CONSTRAINT products_price_fkey
+        FOREIGN KEY (price_id)
+            REFERENCES money(id)
 );

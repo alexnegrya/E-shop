d3d691f175eea640b3320cc111bb4cfd6fc96777
alexnegrya <alexnegrya@mail.ru>
2021-09-09 16:02:40 +0300

    scripts was moved to another folder

--- sql/clients.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE clients
-(
-    id serial,
-    full_name varchar(50),
-    created timestamp,
-    updated timestamp,
-    -- CONSTRAINTS/KEYS
-    CONSTRAINT clients_pkey PRIMARY KEY (id)
-);
--- sql/select.sql
+++
@@ -1,5 +0,0 @@
--- Need to specify table because categories and products have "name" column
-SELECT categories.name, products.name, amount, currency_code
-FROM products
-JOIN categories ON products.category_id = categories.id
-JOIN money ON products.price_id = money.id;

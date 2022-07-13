114e1e7098885ab8df4211a7144e9ec083c6bf14
alexnegrya <alexnegrya@mail.ru>
2022-07-13 09:58:06 +0300

    clients table changed

--- sql/tables/clients.sql
+++ sql/tables/clients.sql
@@ -1,10 +1,13 @@
 CREATE TABLE clients
 (
     id int GENERATED ALWAYS AS IDENTITY,
-    full_name varchar(50),
+    email varchar(200) UNIQUE,
+    first_name varchar(150),
+    last_name varchar(150),
+    password varchar(150),
     created timestamp,
     updated timestamp,
-    address_id integer NOT NULL,
+    address_id integer,
     -- CONSTRAINTS/KEYS
     CONSTRAINT clients_pkey PRIMARY KEY (id),
     CONSTRAINT clients_address_fkey

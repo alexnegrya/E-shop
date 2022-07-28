81663733f98995c32cd07ccce855d045d53e7c7f
alexnegrya <alexnegrya@mail.ru>
2022-07-28 16:23:35 +0300

    paginate function refactoring

--- cli/paginator.py
+++ cli/paginator.py
@@ -1,49 +1,34 @@
-def paginate(l, title=None):
-    from os import system
-    from math import ceil
+from os import system
+from math import ceil
 
-    # help
-    h = 'Enter \"<\" or \">\" to go to the previous or next page\
+
+def paginate(l: tuple | list, title=None):
+    help = 'Enter \"<\" or \">\" to go to the previous or next page\
  and \"p\" to go the specified page.\
  \nEnter \"buy\" to add product to cart or \"back\" to return to the main menu.'
-
-    # hint
     hint = True
-
-    # paginator data
     per_page = 5
     current_page = 1
     page_total = ceil(len(l) / per_page)
-    page_min = (len(l)) - (len(l)) + 1
 
     while True:
         system('clear')
-
         start_i = (current_page - 1) * per_page
         end_i = start_i + per_page
 
-        # print list
-        if title == None:
-            print('List:\n')
-        else:
-            if type(title) == str:
-                print(f'{title}:\n')
-            else:
-                raise TypeError('title must be str type')
-        for i in range(len(l)):
-            if i in range(start_i, end_i):
-                print(l[i])
+        # Print title and content
+        if title == None: print('List:\n')
+        elif type(title) == str: print(f'{title}:\n')
+        else: raise TypeError('title must be str type')
+        [print(l[i]) for i in l[start_i:end_i]]
         print()
 
-        # print pages
+        # Print pages
         for page in range(1, page_total + 1):
-            if page == current_page:
-                print(f'[{page}]', end=' ')
-            else:
-                print(page, end=' ')
+            print(f'[{page}]', end=' ') if page == current_page else print(page, end=' ')
         print()
 
-        # interaction
+        # Interaction
         if hint == True:
             print('Enter \"help\" to more information')
             hint = False
@@ -52,26 +37,17 @@ def paginate(l, title=None):
 
         if enter == 'p':
             print('Enter the number of the page you want to go')
-            choose = int(input('\n>>> '))
-            if choose < page_min:
-                current_page = page_min
-            elif choose > page_total:
-                current_page = page_total
-            else:
-                current_page = choose
+            try: choice = int(input('\n>>> '))
+            except ValueError: continue
+            current_page = 1 if choice < 1 else page_total if choice > page_total else choice
         elif enter == '<':
-            current_page -= 1
-            if current_page < page_min:
-                current_page = 1
+            if current_page < 1: current_page = 1
+            else: current_page -= 1
         elif enter == '>':
-            current_page += 1
-            if current_page > page_total:
-                current_page = page_total
+            if current_page > page_total: current_page = page_total
+            else: current_page += 1
         elif enter == 'help':
             system('clear')
-            print(h)
+            print(help)
             input('\nPress [Enter] to continue... ')
-        elif enter == 'back':
-            return False
-        elif enter == 'buy':
-            return True
+        elif enter in ('back', 'buy'): return enter == 'buy'

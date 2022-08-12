ef9245225971b0e654045c5ea6385edb6d71f0a3
alexnegrya <alexnegrya@mail.ru>
2022-08-12 13:18:05 +0300

    bugs fixes and new keyword formatter update

--- services/pg.py
+++ services/pg.py
@@ -1,5 +1,7 @@
 import psycopg2
 import re
+from psycopg2.errors import InFailedSqlTransaction
+from datetime import datetime
 
 
 class PostgresDataService:
@@ -18,7 +20,8 @@ class PostgresDataService:
         )
         
     def __format_value_for_query(self, value) -> str:
-        return value if type(value) != str else "'" + value.replace("'", "''") + "'" if value != None else 'null'
+        value = str(value) if type(value) not in (str, datetime) else "'" + str(value).replace("'", "''") + "'"
+        return value if value != 'None' else 'null'
 
     def __format_alias(self, value: str) -> str:
         spl = value.split('|')
@@ -28,34 +31,43 @@ class PostgresDataService:
         if name == 'where':
             if type(kwd) == str: return f' WHERE {kwd}'
             elif type(kwd) == dict and all([type(k) == str for k in kwd.keys()]):
-                return f" WHERE {' AND '.join([f'{field} = {value}' for field, value in kwd.items()])}"
+                return f" WHERE {' AND '.join([f'{field} = {self.__format_value_for_query(value)}' for field, value in kwd.items()])}"
             else: return ''
         elif name == 'join':
             if type(kwd) == str:
                 with kwd.split(" ON ") as spl: return f' JOIN {self.__format_alias(spl[0])} ON {spl[1]} ' if len(spl) == 2 else ''
             else: return ''
+        elif name == 'returning':
+            if type(kwd) != tuple: raise TypeError('returning kwd must be a tuple')
+            elif any([type(v) != str for v in kwd]): raise ValueError('returning kwd tuple must contains str fields')
+            else: return f' RETURNING {", ".join([self.__format_alias(rv) for rv in kwd])}'
         else: raise ValueError('unknown keyword')
     
     def query(self, query: str) -> list[tuple]:
-        cursor = self.conn.cursor()
-        if len(re.findall(';', query)) <= 1: cursor.execute(query)
-        else: cursor.executemany(query)
-        self.conn.commit()
         q = query.lower()
-        return cursor.fetchall() if q.startswith('select') or 'returning' in q else []
+        while True:
+            try:
+                cursor = self.conn.cursor()
+                if len(re.findall(r';(\s*[a-zA-Z]+\s+)', query)) <= 1: cursor.execute(query)
+                else: cursor.executemany(query)
+            except InFailedSqlTransaction:
+                self.conn.rollback()
+                continue
+            self.conn.commit()
+            return cursor.fetchall() if q.startswith('select') or 'returning' in q else []
 
     def insert(self, into_table: str, *returning, **values) -> list[tuple]:
         values = {k: self.__format_value_for_query(v) for k, v in values.items()}
-        returning = f' RETURNING {", ".join([self.__format_alias(rv) for rv in returning])}' if type(returning) == str else ''
-        return self.query(f"INSERT INTO {into_table}({', '.join(values.keys())}) VALUES ({', '.join(values.values())}){returning};")
+        return self.query(f"INSERT INTO {into_table}({', '.join(values.keys())})\
+ VALUES ({', '.join([str(value) for value in values.values()])}){self.__format_keyword('returning', returning)};")
 
     def update(self, table: str, where=None, **values) -> None:
-        values = ', '.join([f'{k} = {v}' for k, v in values.items()])
+        values = ', '.join([f'{k} = {self.__format_value_for_query(v)}' for k, v in values.items()])
         self.query(f"UPDATE {table} SET {values}{self.__format_keyword('where', where)};")
 
     def select(self, *fields: tuple[str], from_table: str, join=None, where=None) -> list[dict]:
-        data = self.query(f"SELECT {', '.join(fields)} FROM {self.__format_alias(from_table)} \
-            {''.join([self.__format_keyword(name, kwd) for name, kwd in (('where', where), ('join', join))])}")
+        data = self.query(f"SELECT {', '.join(fields)} FROM {self.__format_alias(from_table)}\
+{''.join([self.__format_keyword(name, kwd) for name, kwd in (('where', where), ('join', join))])}")
         return [{fields[i]: v for i, v in enumerate(row)} for row in data]
 
     def delete(self, from_table: str, where=None) -> None: self.query(f"DELETE FROM {from_table}{self.__format_keyword('where', where)}")

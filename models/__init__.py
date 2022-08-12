a9ff5d5029c6e591d245b8c9abab522491770779
alexnegrya <alexnegrya@mail.ru>
2022-08-12 13:14:39 +0300

    all models and managers added for all import

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,32 @@
+from .addresses import *
+from .categories import *
+from .clients import *
+from .currencies import *
+from .money import *
+from .orders_items import *
+from .orders import *
+from .payments import *
+from .products import *
+from .ratings import *
+from .services import *
+from .shops import *
+from .stock_items import *
+from .contacts import *
 
+
+__all__ = [
+    'Address', 'AddressesManager',
+    'Category', 'CategoriesManager',
+    'Client', 'ClientsManager',
+    'Currency', 'CurrenciesManager',
+    'Money', 'MoneyManager',
+    'OrderItem', 'OrdersItemsManager',
+    'Order', 'OrdersManager',
+    'Payment', 'PaymentsManager',
+    'Product', 'ProductsManager',
+    'Rating', 'RatingsManager',
+    'Service', 'ServicesManager',
+    'Shop', 'ShopsManager',
+    'StockItem', 'StockItemsManager',
+    'Contact', 'ContactsManager'
+]
